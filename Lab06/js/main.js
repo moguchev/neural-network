9c986da1d9cf6bb77f19d50f054c7aa30c9c880a
moguchev <abracadabra.14@mail.ru>
2020-04-13 19:33:32 +0300

    GUI

--- Lab06/js/main.js
+++ Lab06/js/main.js
@@ -1,13 +1,51 @@
-var canvas = document.getElementById('field');
+const canvas = document.getElementById('field');
+const ctx = canvas.getContext('2d');
 
-if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-    
-    for (i = 10; i < 100; i += 10) {
-        drawPoint(ctx, i, i + Math.random()*100, "red")
-    }
+const addPoints = document.getElementById('add-points');
+addPoints.addEventListener('submit', this.addPoint.bind(this), false)
+
+const addCenters = document.getElementById('add-centers');
+addCenters.addEventListener('submit', this.addCenter.bind(this), false)
+
+var POINTS = [];
+var CENTERS = [];
+
+/**
+ * Обработчик формы добавления точки
+ * @param {Event} event
+ */
+function addCenter(event) {
+    event.preventDefault();
+    let x = document.getElementById('xc');
+    let y = document.getElementById('yc');
+    drawСenter(ctx, x.value, y.value, "red")
 }
 
+/**
+ * Обработчик формы добавления точки
+ * @param {Event} event
+ */
+function addPoint(event) {
+    event.preventDefault();
+    let x = document.getElementById('x');
+    let y = document.getElementById('y');
+    drawPoint(ctx, x.value, y.value, "black")
+}
+
+/**
+ * функция отрисовки центра
+ * @param {CanvasRenderingContext2D} ctx контекст canvas
+ * @param {number} x координата центра точки
+ * @param {number} y координата центра точки
+ * @returns {string} цвет точки
+ */
+function drawСenter(ctx, x, y, color) {
+    let rectangle = new Path2D();
+    rectangle.rect(x, y, 10, 10);
+    ctx.fillStyle = color;
+    ctx.fill(rectangle);
+    rectangle.closePath()
+}
 
 /**
  * функция отрисовки точки
@@ -17,7 +55,7 @@ if (canvas.getContext) {
  * @returns {string} цвет точки
  */
 function drawPoint(ctx, x, y, color) {
-    var circle = new Path2D();
+    let circle = new Path2D();
     circle.arc(x, y, 5, 0, 2 * Math.PI);
     ctx.fillStyle = color;
     ctx.fill(circle);
